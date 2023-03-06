9af9dfd5f4a57a6a1082d5484105b02697b16ad2
Day_Fay <danialmukash@gmail.com>
2023-03-06 17:23:53 +0600

    com

---
+++ TSISVI/1.py
@@ -0,0 +1,19 @@
+import os
+
+# Specified path
+path = "/path/to/directory"
+
+# Get list of directories present in the specified path
+dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
+# Get list of files present in the specified path
+files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+# Print the list of directories and files
+print("List of Directories:")
+for d in dirs:
+    print(d)
+
+print("\nList of Files:")
+for f in files:
+    print(f)
